96d5293d0336ba1a499e7ed0735d06e21e1a5dbb
jerry <jerry.cho.dev@hotmail.com>
2023-07-07 17:03:41 +0100

    Clean code

--- app/src/test/java/com/jerry/clean_architecture_mvvm/viewmodel/ContentListViewModelTest.kt
+++ app/src/test/java/com/jerry/clean_architecture_mvvm/viewmodel/ContentListViewModelTest.kt
@@ -1,172 +1,93 @@
 package com.jerry.clean_architecture_mvvm.viewmodel
 
-import app.cash.turbine.test
-import com.jerry.clean_architecture_mvvm.data.source.network.ContentApiService
 import com.jerry.clean_architecture_mvvm.domain.entities.ContentListResponse
-import com.jerry.clean_architecture_mvvm.domain.respository.ContentRepository
-import com.jerry.clean_architecture_mvvm.domain.usecase.impl.GetContentUseCaseImpl
+import com.jerry.clean_architecture_mvvm.domain.usecase.GetContentUseCase
 import com.jerry.clean_architecture_mvvm.presentation.base.ViewState
 import com.jerry.clean_architecture_mvvm.presentation.content.ContentListViewModel
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.HiltTestApplication
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Assert
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.given
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
-import org.robolectric.annotation.LooperMode
-import javax.inject.Inject
-import kotlin.time.ExperimentalTime
+import org.mockito.kotlin.whenever
+import java.net.SocketTimeoutException
 
-@HiltAndroidTest
-@RunWith(RobolectricTestRunner::class)
-@Config(sdk = [25], application = HiltTestApplication::class)
 @ExperimentalCoroutinesApi
-@LooperMode(LooperMode.Mode.PAUSED)
+@RunWith(MockitoJUnitRunner::class)
 class ContentListViewModelTest {
 
-    private val dispatcher = UnconfinedTestDispatcher()
-
-    @get:Rule
-    var hiltRule = HiltAndroidRule(this)
-
-    @Inject
-    lateinit var contentApiService: ContentApiService
-
-    @Inject
-    lateinit var contentRepository: ContentRepository
-
-    @Inject
-    lateinit var getContentUseCaseImpl: GetContentUseCaseImpl
-
-    lateinit var viewModel: ContentListViewModel
+    private lateinit var viewModel: ContentListViewModel
+    private lateinit var getContentUseCase: GetContentUseCase
+    private val testDispatcher = TestCoroutineDispatcher()
 
     @Before
-    fun setup() {
-        // Initialize Hilt
-        hiltRule.inject()
-        Dispatchers.setMain(dispatcher)
-        viewModel = ContentListViewModel(getContentUseCaseImpl, TestCoroutineDispatcher())
-    }
-
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
-    }
-
-    private val testScope = TestCoroutineScope(dispatcher)
-
-    @Test
-    fun `get content usecase success`() = runTest {
-
-        val job = launch {
-            // Force my flow to update via collect invocation
-            val list = viewModel.state.toList()
-
-            println("list::")
-
-            viewModel.getContent()
-
-        }
-
-        // immediately cancel job
-        job.cancel()
-
-        //action
-
-
+    fun setUp() {
+        getContentUseCase = Mockito.mock(GetContentUseCase::class.java)
+        viewModel = ContentListViewModel(getContentUseCase, testDispatcher)
     }
 
     @Test
-    fun `get content usecase success_2`() = runBlockingTest {
-        launch {
-            //assign
-            given(contentApiService.getContentList()).willReturn(getContentListResponse())
+    fun `test content view model ui state expected state`() = runTest {
+        // Prepare the test data
+        given(getContentUseCase()).willReturn(getContentListResponse())
 
-            val sss = viewModel.state.toList()
-            println(sss.size)
-//            viewModel.state.toList() test {
-//                print (awaitItem())
-//                Assert.assertEquals(ViewState.Initial, awaitItem())
-//                Assert.assertEquals(ViewState.Loading, awaitItem())
-//            }
+        val event = mutableListOf<ViewState<ContentListResponse>>()
+        viewModel.state.onEach {
+            event.add(it)
+        }.launchIn(
+            CoroutineScope(UnconfinedTestDispatcher(testScheduler))
+        )
+        viewModel.getContent()
 
-            //action
-            viewModel.getContent()
-        }
+        advanceUntilIdle()
+
+        Assert.assertEquals(3, event.size)
+
+        Assert.assertEquals(ViewState.Initial, event[0])
+        Assert.assertEquals(ViewState.Loading, event[1])
+
+        Assert.assertEquals( 0, (event[2] as ViewState.Success<ContentListResponse>).data.items!!.size )
     }
 
     @Test
-    fun `get content usecase success2`() {
-        runTest {
-            launch {
-                //assign
-                given(contentApiService.getContentList()).willReturn(getContentListResponse())
-
-                viewModel.state.test {
-                    Assert.assertEquals(ViewState.Initial, awaitItem())
-                    Assert.assertEquals(ViewState.Loading, awaitItem())
-
-
-                    println (awaitItem())
-
-
-//                Assert.assertEquals(true, awaitItem().isLoading == null )
-//
-//                Assert.assertEquals(true,awaitItem().isLoading)
-//
-//                Assert.assertEquals(getContentListResponse().items!!.size, awaitItem().data?.items?.size)
-//                Assert.assertEquals(false,awaitItem().isLoading)
-
-                    awaitComplete()
-                }
-
-                //action
-                viewModel.getContent()
-            }
-
+    fun `test content view model ui state expected state with error flow`() = runTest {
+        // Prepare the test data
+        given(getContentUseCase()).willAnswer {
+            throw SocketTimeoutException()
         }
-//        runBlockingTest  {
-//
-//                given(contentApiService.getContentList()).willReturn(getContentListResponse())
-//
-//                //fire
-//                viewModel.getContent()
-//                val list = viewModel.state.toList()
-//
-////                Assert.assertEquals(3, list.size)
-////
-////                Assert.assertEquals(true, list[0].isLoading)
-////                Assert.assertEquals(
-////                    getContentListResponse().items!!.size,
-////                    list[1].data?.items?.size
-////                )
-////                Assert.assertEquals(false, list[2].isLoading)
-//            }
 
+        val event = mutableListOf<ViewState<ContentListResponse>>()
+        viewModel.state.onEach {
+            event.add(it)
+        }.launchIn(
+            CoroutineScope(UnconfinedTestDispatcher(testScheduler))
+        )
+        viewModel.getContent()
+
+        advanceUntilIdle()
+
+        Assert.assertEquals(3, event.size)
+
+        Assert.assertEquals(ViewState.Initial, event[0])
+        Assert.assertEquals(ViewState.Loading, event[1])
+
+        Assert.assertEquals( SocketTimeoutException::class.java.simpleName, (event[2] as ViewState.Failure).t.javaClass.simpleName )
     }
 
-    //"{\"items\":[{\"id\":36,\"title\":\"Article 8\",\"subtitle\":\"Article 8 subtitle with placeholder text\",\"date\":\"18/04/2013 11:48\"},{\"id\":35,\"title\":\"Article 7\",\"subtitle\":\"Article 7 subtitle with placeholder text\",\"date\":\"17/04/2013 11:48\"},{\"id\":34,\"title\":\"Article 6\",\"subtitle\":\"Article 6 subtitle with placeholder text\",\"date\":\"16/04/2013 11:48\"},{\"id\":33,\"title\":\"Article 5\",\"subtitle\":\"Article 5 subtitle with placeholder text\",\"date\":\"15/04/2013 11:48\"},{\"id\":32,\"title\":\"Article 4\",\"subtitle\":\"Article 4 subtitle with placeholder text\",\"date\":\"14/04/2013 11:48\"},{\"id\":31,\"title\":\"Article 3\",\"subtitle\":\"Article 3 subtitle with placeholder text\",\"date\":\"13/04/2013 11:48\"},{\"id\":30,\"title\":\"Article 2\",\"subtitle\":\"Article 2 subtitle with placeholder text\",\"date\":\"12/04/2013 11:48\"},{\"id\":29,\"title\":\"Article 1\",\"subtitle\":\"Article 1 subtitle with placeholder text\",\"date\":\"11/04/2013 11:48\"}]}"
     private fun getContentListResponse() : ContentListResponse {
         return ContentListResponse(
             items = emptyList()
         )
     }
+
 }
